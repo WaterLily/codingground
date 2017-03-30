da614c3c3a35493076743f8bfdd53970ca3434e2
WaterLily <WaterLily@users.noreply.github.com>
2017-03-29 21:27:37 -0700

    Created by Coding Ground Client

---
+++ New Project-20170330/WindowView.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class WindowView implements View {
+
+
+  public void start() {
+      JFrame window = new JFrame("Genetic Music");
+      window.show();
+  }
+  
+  public void display(Model.Monster monster {
+      
+  }
+}
